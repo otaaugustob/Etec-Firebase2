0a8c5cced2b3d540aa7a5df78d87eae27c3f8595
Otavio Bottin <otavioabottin@outlook.com>
2019-05-21 22:30:30 -0300

    Etec Firebase2 - Ex.1

---
+++ app/src/main/java/br/com/etechoracio/firebase/holder/LineHolder.java
@@ -0,0 +1,21 @@
+package br.com.etechoracio.firebase.holder;
+
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import br.com.etechoracio.firebase.R;
+
+public class LineHolder extends RecyclerView.ViewHolder {
+    private TextView viewNome;
+    private ImageView imgDelete;
+
+    public LineHolder(@NonNull View itemView) {
+        super(itemView);
+        viewNome = itemView.findViewById(R.id.itemViewNomeBD);
+        imgDelete = itemView.findViewById(R.id.btnDelete);
+    }
+}
